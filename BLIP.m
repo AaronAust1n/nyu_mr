e9221f4285f18524d8dc75bd55ad6ca832aae5d8
Jakob Aßländer <jakob.asslaender@uniklinik-freiburg.de>
2015-03-10 16:52:35 +0100

    This version projects the images on the real axis, i.e. a dictionary with negative values at the beginning must be fed.

--- BLIP.m
+++ BLIP.m
@@ -1,21 +1,23 @@
-function [T1, PD, z] = BLIP(nuFFT, recon_dim, data, TR, T1_dict, m_dict, mask, z)
+function [T1, PD, z] = BLIP(nuFFT, sparseMatrices, recon_dim, data, T1_dict, m_dict, n_iter, kappa, mask, z)
 
-
-n_iter = 100;
-kappa = 2;
 
 % for display
-if recon_dim(3) < 51
-    slices = 5:2:11;
-else
-    slices = 11:10:51;
-end
+slices = 7:2:13;
+t = [1:2:20, size(data,2)];
 
-if nargin < 8 || isempty(z)
+if nargin < 10 || isempty(z)
     z = zeros(recon_dim);
 end
-
-if isempty(m_dict)
+if nargin < 9 || isempty(mask)
+    mask = 1;
+end
+if nargin < 8 || isempty(kappa)
+    kappa = 1;
+end
+if nargin < 7 || isempty(n_iter)
+    n_iter = 100;
+end
+if nargin < 6 || isempty(m_dict)
     maps = ones(prod(recon_dim(1:end-1)),3);
 end
 
@@ -23,18 +25,21 @@ z_data = 0*data;
 z_old  = z;
 
 for j = 1:n_iter
+    tic;
     % match images to data for each timeframe
     
     z_data_old = z_data;
     
-    parfor l=1:recon_dim(end)
-        nuFFT_l = change_trajectory(nuFFT, l);
-%         if length(recon_dim) == 3
-%             z_data(:,l) = nuFFT_l * z(:,:,l);
-        if length(recon_dim) == 4
-            z_data(:,l) = nuFFT_l * z(:,:,:,l);
-        else
-            error('Images must be either 2D or 3D');
+    if max(abs(z(:))) > 0 % saves comp time if the initial z is 0
+        for l=1:recon_dim(end)
+            nuFFT = change_trajectory(nuFFT, sparseMatrices{l});
+            if length(recon_dim) == 3
+                z_data(:,l) = nuFFT * z(:,:,l);
+            elseif length(recon_dim) == 4
+                z_data(:,l) = nuFFT * z(:,:,:,l);
+            else
+                error('Images must be either 2D or 3D');
+            end
         end
     end
     
@@ -44,38 +49,34 @@ for j = 1:n_iter
     z_old = z;
     
     data_error = 0;
-    parfor l=1:recon_dim(end)
+    for l=1:recon_dim(end)
         tmp = data(:,l) - z_data(:,l);
-        data_error = data_error + makesos(tmp);
+        data_error = data_error + makesos(tmp).^2;
         
-        nuFFT_l = change_trajectory(nuFFT, l);
-%         if length(recon_dim) == 3
-%             z(:,:,l) = mask .* (z(:,:,l) + mu * (nuFFT_l' * tmp));
-        if length(recon_dim) == 4
-%             tmp = z(:,:,:,l);
-            z(:,:,:,l) = mask .* (z(:,:,:,l) + mu * (nuFFT_l' * tmp));
+        nuFFT = change_trajectory(nuFFT, sparseMatrices{l});
+        if length(recon_dim) == 3
+            z(:,:,l) = mask .* (z(:,:,l) + mu * (nuFFT' * tmp));
+        elseif length(recon_dim) == 4
+            z(:,:,:,l) = mask .* (z(:,:,:,l) + mu * (nuFFT' * tmp));
         else
             error('Images must be either 2D or 3D');
         end
     end
-    display(['Data error = ', num2str(data_error)]);
+    display(['Data error = ', num2str(sqrt(data_error)./makesos(data(:)))]);
     
     
     if length(recon_dim) == 3
-        figure(4234); imagesc(array2mosaic(abs(z(:,:,1:100:720))))
+        sfig(4234); subplot(2,1,1); imagesc(array2mosaic(abs(z(:,:,t)))); 
+        sfig(4234); subplot(2,1,2); imagesc(array2mosaic(angle(z(:,:,t)))); colormap jet;
     elseif length(recon_dim) == 4
-        tmp = real(z(:,:,slices,1:100:720));
-        tmp = array2mosaic(tmp(:,:,:), [8 length(slices)]);
-        figure(4234); imagesc(tmp); colorbar;
-        title('real');
-        tmp = imag(z(:,:,slices,1:100:720));
-        tmp = array2mosaic(tmp(:,:,:), [8 length(slices)]);
-        figure(4234234); imagesc(tmp); colorbar;
-        title('imag');
+        tmp = z(:,:,slices,t);
+        tmp = array2mosaic(tmp(:,:,:), [length(t) length(slices)]);
+        sfig(09568756); imagesc(angle(tmp)); colormap jet
+        sfig(4234); imagesc(abs(tmp));
     else
         error('Images must be either 2D or 3D');
     end
-    title(['iteration = ', num2str(j-.5), '; mu = ', num2str(mu)]); drawnow;
+        title(['iteration = ', num2str(j-.5), '; mu = ', num2str(mu)]); drawnow;
     
     
     z = reshape(z, [prod(recon_dim(1:end-1)), recon_dim(end)]);
@@ -100,40 +101,45 @@ for j = 1:n_iter
         PD = maps(:,:,2)-maps(:,:,1);
         T1 = maps(:,:,3) .* (maps(:,:,2)./maps(:,:,1) - 1);
     else
+%         tmp = abs(z);
+%         tmp = tmp .* sign(real(z));
+%         tmp = tmp * m_dict;
         tmp = real(z) * m_dict;
+        tmp(tmp<0) = 0;
         tmp = tmp ./ repmat(makesos(m_dict,1),    [size(tmp,1) 1]);
         [~, idx] = max(tmp, [], 2);
         tmp = tmp ./ repmat(makesos(m_dict,1),    [size(tmp,1) 1]);
         for n=prod(recon_dim(1:end-1)):-1:1
-            PD(n) = real(tmp(n,idx(n)));
-            z(n,:) = PD(n) .* m_dict(:,idx(n)).';
+            PD(n) = tmp(n,idx(n));
+            z(n,:) = PD(n) .* m_dict(:,idx(n)).'; % + 1i * imag(z(n,:));
+%             z(n,:) = abs(PD(n) .* m_dict(:,idx(n)).') .* exp(1i * angle(z(n,:)));
             T1(n) = T1_dict(idx(n));
         end
         z  = reshape(z,  recon_dim);
         PD = reshape(PD, recon_dim(1:end-1));
         T1 = reshape(T1, recon_dim(1:end-1));
-        
-        %         figure(2); hold all; plot(squeeze(z(40,27,:)));
     end
     
     if length(recon_dim) == 3
-        figure(234); imagesc(array2mosaic(abs(z(:,:,1:100:720)))); title(['iteration = ', num2str(j)]);
-        figure(12342); subplot(2,1,1); imagesc(PD); colormap jet; colorbar
+        sfig(234); subplot(2,1,1); imagesc(array2mosaic(real(z(:,:,t)))); title(['iteration = ', num2str(j)]);colorbar
+        sfig(234); subplot(2,1,2); imagesc(array2mosaic(imag(z(:,:,t)))); title(['imag']); colorbar
+        sfig(12342); subplot(2,1,1); imagesc(PD); colormap jet; colorbar
         title('PD [a.u.]');
-        figure(12342); subplot(2,1,2); imagesc(T1, [0 5]); colormap jet; colorbar
+        sfig(12342); subplot(2,1,2); imagesc(T1); colormap jet; colorbar
         title('T1 [s]');
     elseif length(recon_dim) == 4
-        tmp = abs(z(:,:,slices,1:100:720));
-        tmp = array2mosaic(tmp(:,:,:), [8 length(slices)]);
-        figure(234); imagesc(tmp); title(['iteration = ', num2str(j)]);
-        figure(12342); subplot(2,1,1); imagesc(array2mosaic(PD(:,:,slices))); colormap jet; colorbar
+        tmp = abs(z(:,:,slices,t));
+        tmp = array2mosaic(tmp(:,:,:), [length(t) length(slices)]);
+        sfig(234); imagesc(tmp); title(['iteration = ', num2str(j)]);
+        sfig(12342); subplot(2,1,1); imagesc(array2mosaic(PD(:,:,slices))); colormap jet; colorbar
         title('PD [a.u.]');
-        figure(12342); subplot(2,1,2); imagesc(array2mosaic(T1(:,:,slices)), [0 5]); colormap jet; colorbar
+        sfig(12342); subplot(2,1,2); imagesc(array2mosaic(T1(:,:,slices)), [0 3]); colormap jet; colorbar
         title('T1 [s]');
     else
         error('Images must be either 2D or 3D');
     end
     drawnow;
+    toc
 end
 
 end
